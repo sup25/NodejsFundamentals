68d5508bd271d25003f44653d0f9a7e471e4c90f
sup25 <suparnaa51@gmail.com>
2023-02-25 21:53:45 +0545

    update

---
+++ pathmodule/path.js
@@ -0,0 +1,9 @@
+const path = require('path')
+//to see the directory name
+console.log(path.dirname('D:/nodejs/pathmodule/path.js'))
+//to see extension name
+console.log(path.extname('D:/nodejs/pathmodule/path.js'))
+//to see path name
+console.log(path.basename('D:/nodejs/pathmodule/path.js'))
+//to know all in one 
+console.log(path.parse('D:/nodejs/pathmodule/path.js'))
