4a2e2f3594dc5f796d571a56d26e26edae0360a3
sup25 <suparnaa51@gmail.com>
2023-02-28 21:05:27 +0545

    update

---
+++ npmMod/index.js
@@ -0,0 +1,9 @@
+//using npm modules
+// import chalk from 'chalk';
+// console.log(chalk.blue.inverse('Hello World'));
+
+
+import chalk from 'chalk';
+import validator from 'validator';
+const res = validator.isEmail('foo@bar.io'); //=> true
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res))
